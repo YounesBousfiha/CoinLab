f67cd69d304a1442384b3f946dc9de5be4fd32a3
Coderyounes <younesanas2021@gmail.com>
2025-09-23 09:37:55 +0100

    FEAT(COIN-34): implement singleton Database class for database connection management

---
+++ src/infrastructure/config/Database.java
@@ -0,0 +1,40 @@
+package infrastructure.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+
+    private static Database instance;
+    private final Connection connection;
+
+
+    private Database() throws SQLException {
+        try {
+            String url = System.getenv("DB_URL");
+            String user = System.getenv("DB_USER");
+            String password = System.getenv("DB_PASSWORD");
+            this.connection = DriverManager.getConnection(url, user, password);
+        }  catch (SQLException e) {
+            throw new SQLException("could connect to Database : ", e);
+        }
+    }
+
+
+    public static Database getInstance() throws SQLException {
+        try {
+            if(instance == null || instance.getConnection().isClosed()) {
+                instance = new Database();
+            }
+        } catch (SQLException e) {
+            throw new SQLException("couldn't connect to database : ", e);
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return this.connection;
+    }
+}
