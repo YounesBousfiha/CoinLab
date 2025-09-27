9d099dd9cf5f753a825b1776ed33e9c92a7b6122
Coderyounes <younesanas2021@gmail.com>
2025-09-27 13:57:00 +0100

    FEAT: enhance WalletRepoImpl with conflict resolution for wallet insertion and improve code readability

--- src/infrastructure/persistance/WalletRepoImpl.java
+++ src/infrastructure/persistance/WalletRepoImpl.java
@@ -15,6 +15,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WalletRepoImpl implements WalletRepository {
+
+    private static final String ID_COLUMN = "id";
+    private static final String BALANCE_COLUMN = "balance";
+    private static final String TYPE_COLUMN = "type";
+    private static final String ADDRESS_COLUMN = "address";
+    private static final String CREATED_AT_COLUMN = "created_at";
+
     private static final Logger logger = LoggerFactory.getLogger(WalletRepoImpl.class);
 
     private final Connection db;
@@ -25,7 +32,11 @@ public class WalletRepoImpl implements WalletRepository {
 
     @Override
     public Wallet save(Wallet wallet) {
-        String sql = "INSERT INTO wallet (balance, type, address) VALUES (?, ?::wallet_type, ?) RETURNING id, created_at, type, address, balance";
+        String sql = "INSERT INTO wallet (balance, type, address)" +
+                "VALUES (?, ?::wallet_type, ?)" +
+                 "ON CONFLICT (address) DO UPDATE "+
+                "SET balance = excluded.balance "+
+                "RETURNING id, created_at, type, address, balance";
         try (PreparedStatement stmt = db.prepareStatement(sql)) {
             stmt.setBigDecimal(1, wallet.getBalance());
             stmt.setString(2, wallet.getType().name());
@@ -33,11 +44,11 @@ public class WalletRepoImpl implements WalletRepository {
 
            try ( ResultSet rs = stmt.executeQuery()) {
                 if(rs.next()) {
-                     wallet.setId(rs.getLong("id"));
-                     wallet.setCreatedAt(rs.getTimestamp("created_at").toLocalDateTime());
-                     wallet.setType(CryptoType.valueOf(rs.getString("type")));
-                     wallet.setAddress(rs.getString("address"));
-                     wallet.setBalance(rs.getBigDecimal("balance"));
+                     wallet.setId(rs.getLong(ID_COLUMN));
+                     wallet.setCreatedAt(rs.getTimestamp(CREATED_AT_COLUMN).toLocalDateTime());
+                     wallet.setType(CryptoType.valueOf(rs.getString(TYPE_COLUMN)));
+                     wallet.setAddress(rs.getString(ADDRESS_COLUMN));
+                     wallet.setBalance(rs.getBigDecimal(BALANCE_COLUMN));
                 }
            }
         } catch (SQLException e) {
@@ -56,17 +67,17 @@ public class WalletRepoImpl implements WalletRepository {
             try (ResultSet rs = stmt.executeQuery()) {
                 if(rs.next()) {
                     Wallet wallet = new Wallet();
-                    wallet.setId(rs.getLong("id"));
-                    wallet.setBalance(rs.getBigDecimal("balance"));
-                    wallet.setType(wallet.getType());
-                    wallet.setAddress(rs.getString("address"));
-                    wallet.setCreatedAt(rs.getTimestamp("created_at").toLocalDateTime());
+                    wallet.setId(rs.getLong(ID_COLUMN));
+                    wallet.setBalance(rs.getBigDecimal(BALANCE_COLUMN));
+                    wallet.setType(CryptoType.valueOf(rs.getString(TYPE_COLUMN)));
+                    wallet.setAddress(rs.getString(ADDRESS_COLUMN));
+                    wallet.setCreatedAt(rs.getTimestamp(CREATED_AT_COLUMN).toLocalDateTime());
 
                     return Optional.of(wallet);
                 }
             }
         } catch (SQLException e) {
-            //logger.error("Error finding wallet by id", e);
+            logger.error("Error finding wallet by id", e);
         }
         return Optional.empty();
     }
@@ -83,7 +94,7 @@ public class WalletRepoImpl implements WalletRepository {
                 }
             }
         } catch (SQLException e) {
-            //logger.error("Error checking wallet address existence", e);
+            logger.error("Error checking wallet address existence", e);
         }
         return false;
     }
