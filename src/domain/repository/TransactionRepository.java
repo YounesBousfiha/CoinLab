da9ed17344a0b667126edcb2230d8da0e03a1a1d
Coderyounes <younesanas2021@gmail.com>
2025-09-27 13:57:15 +0100

    FEAT: enhance TransactionRepoImpl with UUID handling, conflict resolution, and fee level statistics retrieval

--- src/domain/repository/TransactionRepository.java
+++ src/domain/repository/TransactionRepository.java
@@ -1,5 +1,6 @@
 package domain.repository;
 
+import application.dto.FeeLevelStatsDTO;
 import domain.entity.Transaction;
 
 import java.util.List;
@@ -7,9 +8,11 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface TransactionRepository {
-    public void save(Transaction transaction);
+    public Transaction save(Transaction transaction);
     Optional<List<Transaction>> findAllByWalletId(UUID walletId);
     int countPending(); // for mempool service
     Optional<List<Transaction>> findAllPending(); // for mempool service
+    Optional<List<Transaction>> findAllPendingWithLimit(int limit);
+    Optional<List<FeeLevelStatsDTO>>  getFeeLevelStatistics(int limit, int cycleTime);
 
 }
