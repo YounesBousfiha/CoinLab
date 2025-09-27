657739917348a205dc32761354642551cb040571
Coderyounes <younesanas2021@gmail.com>
2025-09-27 13:57:29 +0100

    FEAT: add TransactionMapper for converting between Transaction and TransactionDTO

---
+++ src/application/mapper/TransactionMapper.java
@@ -0,0 +1,40 @@
+package application.mapper;
+
+import application.dto.TransactionDTO;
+import domain.entity.Transaction;
+import domain.enums.Priority;
+
+import java.math.BigDecimal;
+
+public class TransactionMapper {
+
+
+    private TransactionMapper() {}
+
+    public static TransactionDTO toDTO(Transaction transaction) {
+        TransactionDTO transactionDTO = new TransactionDTO();
+        transactionDTO.setId(transaction.getId());
+        transactionDTO.setUuid(transaction.getUuid());
+        transactionDTO.setSource(transaction.getSource());
+        transactionDTO.setDestination(transaction.getDestination());
+        transactionDTO.setAmount(transaction.getAmount());
+        transactionDTO.setFee(transaction.getFee());
+        transactionDTO.setPriority(transaction.getPriority());
+        transactionDTO.setStatus(transaction.getStatus());
+        transactionDTO.setCreatedAt(transaction.getCreatedAt());
+
+        return transactionDTO;
+    }
+
+    public static Transaction toEntity(TransactionDTO transactionDTO) {
+        return new Transaction.Builder()
+                .source(transactionDTO.getSource())
+                .uuid(transactionDTO.getUuid())
+                .destination(transactionDTO.getDestination())
+                .amount(transactionDTO.getAmount())
+                .status(transactionDTO.getStatus())
+                .fee(transactionDTO.getFee())
+                .priority(transactionDTO.getPriority())
+                .build();
+    }
+}
