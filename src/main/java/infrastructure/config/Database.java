e2cd280bf9390b0d1447b02aeb2404adca7a1c67
Coderyounes <younesanas2021@gmail.com>
2025-09-29 07:41:16 +0100

    REFactor: enhance Database connection initialization with properties file and environment variables( to solve SonarCube Blocker)

--- src/main/java/infrastructure/config/Database.java
+++ src/main/java/infrastructure/config/Database.java
@@ -1,8 +1,11 @@
 package infrastructure.config;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Database {
 
@@ -11,14 +14,24 @@ public class Database {
     private final Connection connection;
 
 
-    private Database() throws SQLException {
-        try {
-            String url = GlobalValue.DB_URL;
-            String user = GlobalValue.DB_USER;
-            String password = GlobalValue.PASSWORD;
+    private Database() throws IOException, SQLException {
+        try (InputStream input = Database.class.getClassLoader().getResourceAsStream("application.properties")) {
+            Properties props = new Properties();
+            props.load(input);
+
+            String host = System.getenv().getOrDefault("DB_HOST", props.getProperty("db.host"));
+            String port = System.getenv().getOrDefault("DB_PORT", props.getProperty("db.port"));
+            String dbName = System.getenv().getOrDefault("DB_NAME", props.getProperty("db.dbname"));
+            String user = System.getenv().getOrDefault("DB_USER", props.getProperty("db.user"));
+            String password = System.getenv().getOrDefault("DB_PASSWORD", props.getProperty("db.password"));
+
+            String url = String.format("jdbc:postgresql://%s:%s/%s", host, port, dbName);
+
+
+
             this.connection = DriverManager.getConnection(url, user, password);
-        }  catch (SQLException e) {
-            throw new SQLException("couldn't connect to Database : ", e);
+        } catch (SQLException e) {
+            throw new SQLException("Failed to connect to database", e);
         }
     }
 
@@ -28,7 +41,7 @@ public class Database {
             if(instance == null || instance.getConnection().isClosed()) {
                 instance = new Database();
             }
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             throw new SQLException("couldn't connect to database : ", e);
         }
         return instance;
@@ -38,3 +51,6 @@ public class Database {
         return this.connection;
     }
 }
+
+
+
