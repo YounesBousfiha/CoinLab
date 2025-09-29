8596f523aff04e6c10e29bdf91a795ef07061980
Coderyounes <younesanas2021@gmail.com>
2025-09-29 15:21:49 +0100

    FEAT: remove unused fields and simplify MemPoolImpl class for clarity

--- src/main/java/infrastructure/persistance/MemPoolImpl.java
+++ src/main/java/infrastructure/persistance/MemPoolImpl.java
@@ -1,14 +1,6 @@
 package infrastructure.persistance;
 
-import domain.entity.Transaction;
 import domain.repository.MempoolRepository;
 
-import java.sql.Connection;
 
-public class MemPoolImpl implements MempoolRepository {
-    private final Connection db;
-
-    public MemPoolImpl(Connection db) {
-        this.db = db;
-    }
-}
+public class MemPoolImpl implements MempoolRepository { }
