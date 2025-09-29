bae5c4222213183f6d868908d4a6c78a67f657b3
Coderyounes <younesanas2021@gmail.com>
2025-09-29 07:41:26 +0100

    REFACTOR: comment out sensitive database credentials in GlobalValue

--- src/main/java/infrastructure/config/GlobalValue.java
+++ src/main/java/infrastructure/config/GlobalValue.java
@@ -4,7 +4,7 @@ public class GlobalValue {
 
     private GlobalValue() {}
     public static final boolean USE_DATABASE = true;
-    public static final String DB_URL = "jdbc:postgresql://localhost:5432/coinlab";
-    public static final String DB_USER = "younes";
-    public static final String PASSWORD = "test123";
+    // public static final String DB_URL = "jdbc:postgresql://localhost:5432/coinlab";
+    // public static final String DB_USER = "younes";
+    // public static final String PASSWORD = "test123";
 }
